a7095e9efd1d0a8a4ca6eb3acd3e73dd7af8fc44
Vojta Tomas <hello@vojtatom.cz>
2023-05-31 14:46:42 +0200

    Initial commit

---
+++ vitest.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vitest/config";
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "csvts/csvts.ts"),
+      name: "csvts",
+      // the proper extensions will be added
+      fileName: "csvts",
+    },
+  },
+  resolve: {
+    alias: {
+      "@csvts": resolve(__dirname, "./csvts"),
+    },
+  },
+  plugins: [dts()],
+});
