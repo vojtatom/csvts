db9903f5522949622c03cb0f074117f6022b4a6a
Vojta Tomas <hello@vojtatom.cz>
2023-05-31 15:31:00 +0200

    Readme + bugfix

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # CSV.ts 📄
 
 Tiny utility for parsing CSV files.
+
+-   dead simple, just a few lines of code
+-   no dependencies
+-   written in TypeScript
+
+```
+npm i csvts
+```
+
+## Usage
+
+```ts
+import { stringify, parse } from 'csvts';
+
+const data = [
+    ['id', 'name', 'age', 'weight', 'married', 'children', 'timestamp'],
+    ['1', '"Dan, John"', '30', '80.5', 'true', '2', '2021-01-01T00:00:00.000Z'],
+    ['2', '"Jack"', '40', '90.5', 'false', '0', '2021-01-02T00:00:00.000Z'],
+    ['3', 'Back, "Jim"', '50', '"100.5"', 'true', '3', '2021-01-03T00:00:00.000Z'],
+];
+
+const csv = stringify(csvData);
+const originalData = parse(csv);
+```
--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,21 @@
-import { defineConfig } from "vite";
-import { resolve } from "path";
-import dts from "vite-plugin-dts";
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    lib: {
-      entry: resolve(__dirname, "shpts/csvts.ts"),
-      name: "csvts",
-      // the proper extensions will be added
-      fileName: "csvts",
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'csvts/csvts.ts'),
+            name: 'csvts',
+            // the proper extensions will be added
+            fileName: 'csvts',
+        },
     },
-  },
-  resolve: {
-    alias: {
-      "@csvts": resolve(__dirname, "./csvts"),
+    resolve: {
+        alias: {
+            '@csvts': resolve(__dirname, './csvts'),
+        },
     },
-  },
-  plugins: [dts()],
+    plugins: [dts()],
 });
